760e1a85441ce11c8c3c84617c3b6ab169d6b62a
SatoShinta <kanten3005@gmail.com>
2024-06-16 13:35:10 +0900

    OnTriggerExit2Dにしました

--- Assets/Scripts/YUKAcontroller.cs
+++ Assets/Scripts/YUKAcontroller.cs
@@ -9,7 +9,7 @@ public class YUKAcontroller : MonoBehaviour
     int counter = 0;
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
